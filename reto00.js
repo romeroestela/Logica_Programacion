c3053b53f609cce694a18528dff9711ac7f9bfca
estela.romero.edu <romeroestela3@gmail.com>
2024-04-29 17:25:02 +0200

    Primer commit: Agregar archivos iniciales

---
+++ reto00.js
@@ -0,0 +1,37 @@
+// https://www.javascript.com/ 
+
+//Comentario en una linea
+
+/* Comentario
+en varias
+lineas */
+
+// En javaScript se utiliza camelCase, cada palabra después de la primera comienza con mayúscula, excepto la primera palabra que comienza con minúscula.
+
+var miVariable = "Mi variable"
+miVariable = "Cambiamos valor a la variable"
+
+const miConstante = "Mi constante"
+
+function variables(){
+    let myInt = 1
+    let myFloat = 1.5
+    let myBool = true
+    myBool = false
+    let myNull = null
+    let myString = "Mi cadena de texto"
+    let myOtherString = 'Mi otra cadena de texto'
+
+    console.log(myInt)
+    console.log(myFloat)
+    console.log(myBool)
+    console.log(myNull)
+    console.log(myString)
+    console.log(myOtherString)
+}
+
+variables(); //
+
+console.log(variables)
+
+console.log("¡Hola, JavaScript!")
---
+++ reto01.js
@@ -0,0 +1,14 @@
+/* OPERADORES */
+
+/** ARITMÉTICOS **/
+var num1 = 10
+var num2 = 4 
+
+console.log("Suma: 5 + 5=", 5+5) 
+console.log("Resta: 9 - 5", 9-5)
+console.log("Multiplicación: 9 * 5", 9*5)
+console.log("División: 10 / 2", 10/2)
+console.log("Residuo: 25 % 6", 25%6 )
+console.log("Exponente: 5 ** 3", 5**3)
+console.log("Incremento: ", ++num1)
+console.log("Decremento: ", --num1)
